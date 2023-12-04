4a320eb668e661ec67dfef50c9406918d800f1e1
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-04 09:51:09 -0500

    Specific min python version

--- setup.py
+++ setup.py
@@ -1,6 +1,23 @@
 from setuptools import setup, Extension
-import Cython.Build
+from Cython.Build import build_ext as cython_build_ext
 from wheel.bdist_wheel import bdist_wheel
+from setuptools.command.build_ext import get_abi3_suffix, get_config_var
+
+PY_LIMITED_API = "0x030600f0"
+MIN_PYTHON_VERSION = "3.6"
+CPYTHON_TAG = f"cp{MIN_PYTHON_VERSION.replace('.', '')}"
+
+extensions = [
+    Extension(
+        "adder.add",
+        sources=["adder/add.pyx"],
+        py_limited_api=True,
+        define_macros=[
+            ("Py_LIMITED_API", "0x030600f0"),
+            ("CYTHON_LIMITED_API", "1"),
+        ],
+    )
+]
 
 
 class bdist_wheel_abi3(bdist_wheel):
@@ -8,25 +25,39 @@ class bdist_wheel_abi3(bdist_wheel):
         python, abi, plat = super().get_tag()
 
         if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.6
-            return "cp36", "abi3", plat
+            return CPYTHON_TAG, "abi3", plat
 
         return python, abi, plat
+
+
+class bdist_ext_abi3(cython_build_ext):
+    def get_ext_filename(self, fullname):
+        filename = super().get_ext_filename(fullname)
+        if fullname in self.ext_map:
+            ext = self.ext_map[fullname]
+            use_abi3 = getattr(ext, "py_limited_api") and get_abi3_suffix()
+            if use_abi3:
+                so_ext = get_config_var("EXT_SUFFIX")
+                filename = filename[: -len(so_ext)]
+                filename = filename + get_abi3_suffix()
+        return filename
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.ext_map = {}
+
+    def finalize_options(self):
+        super().finalize_options()
+        for ext in self.extensions:
+            fullname = self.get_ext_fullname(ext.name)
+            self.ext_map[fullname] = ext
+            self.ext_map[fullname.split(".")[-1]] = ext
 
 
 setup(
     name="adder",
     packages=["adder"],
-    ext_modules=[
-        Extension(
-            "adder.add",
-            ["adder/add.pyx"],
-            py_limited_api=True,
-            define_macros=[
-                ("Py_LIMITED_API", "0x030600f0"),
-                ("CYTHON_LIMITED_API", "1"),
-            ],
-        )
-    ],
-    cmdclass={"build_ext": Cython.Build.build_ext, "bdist_wheel": bdist_wheel_abi3},
+    ext_modules=extensions,
+    python_requires=f">={MIN_PYTHON_VERSION}",
+    cmdclass={"build_ext": bdist_ext_abi3, "bdist_wheel": bdist_wheel_abi3},
 )
