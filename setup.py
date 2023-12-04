7038cb772f334dd82756482918ff65f6f585d947
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-03 22:32:14 -0500

    Correct tags

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 from setuptools import setup, Extension
 import Cython.Build
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.7
+            return "cp37", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
     ext_modules=[
@@ -7,11 +20,11 @@ setup(
             "adder.add",
             ["adder/add.pyx"],
             py_limited_api=True,
-            extra_compile_args=[
-                "-DCYTHON_LIMITED_API=1",
-                "-DPy_LIMITED_API=0x030700f0",
+            define_macros=[
+                ("Py_LIMITED_API", "0x030700f0"),
+                ("CYTHON_LIMITED_API", "1"),
             ],
         )
     ],
-    cmdclass={"build_ext": Cython.Build.build_ext},
+    cmdclass={"build_ext": Cython.Build.build_ext, "bdist_wheel": bdist_wheel_abi3},
 )
